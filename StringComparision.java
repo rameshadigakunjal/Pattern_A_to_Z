6bad113fdae75430ea86f18acd4e30b62e03d901
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-11-04 16:09:49 +0530

    second day

---
+++ StringComparision.java
@@ -0,0 +1,12 @@
+class StringComparision {
+    public static void main(String[] args) {
+        String s1 = "JAVA";
+        String s2 = "JAVA";
+
+        if (s1 == s2) {
+            System.out.println("References are Equal");
+        } else {
+            System.out.println("References are not Equal");
+        }
+    }
+}
